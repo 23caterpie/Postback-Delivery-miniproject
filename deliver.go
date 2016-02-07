de165fa9de86755ad9aec49e4b80eff3845d2b41
23caterpie <23caterpie@users.noreply.github.com>
2016-02-07 13:53:40 -0800

    deliver.go\n +Pulls postback objects from the local Redis database via a List.\n +Parses postback objects and delivers them to the end point.\n\t +Supports GET.\n\t +Working on POST.\n +Added logging support for trace, info, warning, and error levels.\n\t Currently logs delivery time, response time, response code, and response body as well as warnings and errors.

--- deliver.go
+++ deliver.go
@@ -1,35 +1,152 @@
 package main
 
-import "fmt"
-import "github.com/garyburd/redigo/redis"
+import (
+		"fmt"
+		"log"
+		"os"
+		"io"
+		"bytes"
+		"strings"
+		"regexp"
+		"flag"
+		"net/http"
+		"io/ioutil"
+		"encoding/json"
+		"github.com/garyburd/redigo/redis"
+)
+
+var (
+	home   = os.Getenv("HOME")
+    user   = os.Getenv("USER")
+    gopath = os.Getenv("GOPATH")
+	
+	curlyBracketMatch = regexp.MustCompile("{.*?}")
+	
+	Trace   *log.Logger
+    Info    *log.Logger
+    Warning *log.Logger
+    Error   *log.Logger
+)
+
+type PostbackObject struct {
+    Method string              `json:"method"`
+    Url    string              `json:"url"`
+	Data   map[string]string   `json:"data"`
+}
+
+func Init() {
+	if user == "" {
+        log.Fatalln("$USER not set")
+    }
+    if home == "" {
+        home = "/home/" + user
+    }
+    if gopath == "" {
+        gopath = home + "/go"
+    }
+    // gopath may be overridden by --gopath flag on command line.
+    flag.StringVar(&gopath, "gopath", gopath, "override default GOPATH")
+}
+
+func InitLoggers(traceHandle io.Writer,
+				 infoHandle io.Writer,
+				 warningHandle io.Writer,
+				 errorHandle io.Writer) {
+	Trace =   log.New(traceHandle,   "TRACE: ",   log.Ldate|log.Lmicroseconds|log.Llongfile)
+    Info =    log.New(infoHandle,    "INFO: ",    log.Ldate|log.Lmicroseconds|log.Llongfile)
+    Warning = log.New(warningHandle, "WARNING: ", log.Ldate|log.Lmicroseconds|log.Llongfile)
+    Error =   log.New(errorHandle,   "ERROR: ",   log.Ldate|log.Lmicroseconds|log.Llongfile)
+}
+
+func replaceUrlWithData(postback *PostbackObject) {
+	stringIndexesMatches := curlyBracketMatch.FindStringIndex(postback.Url)
+	for stringIndexesMatches != nil {
+		bracketMatchString := curlyBracketMatch.FindString(postback.Url)
+		matchString := bracketMatchString[1:(len(bracketMatchString) - 1)]
+		replaceString := postback.Data[matchString]
+		postback.Url = postback.Url[:stringIndexesMatches[0]] + replaceString + postback.Url[stringIndexesMatches[1]:]
+		stringIndexesMatches = curlyBracketMatch.FindStringIndex(postback.Url)
+	}
+}
+
+func logEndpointResponseInfo(response *http.Response, postback PostbackObject) {
+	Info.Println("Received response from: <" + postback.Url + ">")
+	Info.Println("response code:", response.StatusCode)
+	//Info.Println("response headers:", response.Header)
+	body, _ := ioutil.ReadAll(response.Body)
+	Info.Println("response body:", string(body))
+}
+
+func deliverGet(postback PostbackObject) {
+	Info.Println("Delivering. url: <" + postback.Url + "> method: " + postback.Method)
+	response, err := http.Get(postback.Url)
+	if err != nil {
+		Warning.Println("Could not send GET request to: <" + postback.Url + ">")
+	} else {
+		defer response.Body.Close()
+		logEndpointResponseInfo(response, postback)
+	}
+}
+
+func deliverPost(postback PostbackObject) {
+	requestBody, _ := json.Marshal(postback.Data)
+	Trace.Println("requestBody: " + string(requestBody))
+	request, err :=  http.NewRequest("POST", postback.Url, bytes.NewBuffer(requestBody))
+	//request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	Trace.Println("request: " + fmt.Sprint(request))
+	Info.Println("Delivering. url: <" + postback.Url + "> method: " + postback.Method)
+	response, err := client.Do(request)
+	if err != nil {
+		Warning.Println("Could not send POST request to: <" + postback.Url + ">")
+	} else {
+		defer response.Body.Close()
+		logEndpointResponseInfo(response, postback)
+	}
+}
+
+func processPostbackObject(redisServer redis.Conn) {
+	endpoint, err := redis.String(redisServer.Do("LPOP", "request"))
+	if err == nil && endpoint != "" {
+		postback := PostbackObject{}
+		json.Unmarshal([]byte(endpoint), &postback)
+		Trace.Println("endpoint: " + endpoint)
+		Trace.Println("postback: " + fmt.Sprint(postback))
+		replaceUrlWithData(&postback)
+		Trace.Println("postback.Url: " + postback.Url)
+		if strings.ToUpper(postback.Method) == "GET" {
+			deliverGet(postback)
+		} else if strings.ToUpper(postback.Method) == "POST" {
+			deliverPost(postback)
+		} else {
+			Error.Println("Unsupported Postback Method.")
+		}
+	} else if endpoint == "" {
+		//Warning.Println("Received empty Redis Object.")
+	} else {
+		Warning.Println("Redis Problem: " + fmt.Sprint(err))
+	}
+}
 
 func main() {
-	c, err := redis.Dial("tcp", ":6379")
+	logFile, logFileError := os.OpenFile("deliver.go.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if logFileError != nil {
+		log.Fatalln("Failed to open lof file: ", logFileError)
+	}
+	defer logFile.Close()
+
+	traceOutput := os.Stdout //use "ioutil.Discard" to ignore Trace logs
+	infoOutput := io.MultiWriter(logFile, os.Stdout)
+	warningOutput := io.MultiWriter(logFile, os.Stdout)
+	errorOutput := io.MultiWriter(logFile, os.Stderr)
+	InitLoggers(traceOutput, infoOutput, warningOutput, errorOutput)
 	
+	redisServer, err := redis.Dial("tcp", ":6379")
 	if err != nil {
-		log.Fatal(err)
-		panic(err)
+		Error.Fatalln(err)
 	}
-	defer c.Close()
-	
-	if(!redis.Bool(c.Do("EXISTS", "postback_head"))){
-			c.Do("SET", "postback_head", 0)
+	defer redisServer.Close()
+	for {
+		processPostbackObject(redisServer)
 	}
-	
-	for{
-		postback_head := redis.Int(c.Do("GET", "postback_head"))
-		c.Do("GET", "postback_head")
-	}
-
-	//set
-	//c.Do("SET", "message1", "Hello World")
-
-	//get
-	//world, err := redis.String(c.Do("GET", "message1"))
-	
-	//if err != nil {
-	//	fmt.Println("key not found")
-	//}
-
-	fmt.Println(world)
 }
--- ingest.php
+++ ingest.php
@@ -24,10 +24,26 @@
  *  }
  *  
  *  for each "data" object in the accepted request, a "postback" object will be
- *  pushed to a local redis database with the endpoint and data information hash form:
- *  KEY           FIELDS      VALUES
- *  postback:###  "endpoint": {"method":"GET","url":"http:\/\/example-server.com\/data?key={key}&value={value}&foo={bar}"}
- *                "data":     {"key":"Azureus","value":"Dendrobates"}
+ *  appended to the "request" list in a local redis database as such:
+ *  KEY      VALUES
+ *  request  {  
+ *			    "method":"GET",
+ *			    "url":"http://sample-domain-endpoint.com/data?key={key}&value={value}&foo={bar}"
+ *			    "data":
+ *			     {  
+ *			       "key":"Azureus",
+ *			       "value":"Dendrobates"
+ *			     }
+ *           },
+ *           {  
+ *			    "method":"GET",
+ *			    "url":"http://sample-domain-endpoint.com/data?key={key}&value={value}&foo={bar}"
+ *			    "data":
+ *			     {  
+ *			       "key":"Phyllobates",
+ *                 "value":"Terribilis"
+ *			     }
+ *           }
  */
 
 require 'Predis/Autoloader.php';
@@ -38,14 +54,14 @@ Predis\Autoloader::register();
 //tries to decode JSON from the raw post data. If it can not, echos an error
 if($content = json_decode(file_get_contents('php://input'), true))
 {
-	var_dump($content);
+	//var_dump($content);
 	
 	//varifies that the provided endpoint url is in valid form. Else echos an error
 	if(filter_var($content['endpoint']['url'], FILTER_VALIDATE_URL))
 	{
 		//varifies the endpoint method is supported. Else echos an error
-		if($content['endpoint']['method'] == GET ||
-		$content['endpoint']['method'] == POST)
+		if(strtoupper($content['endpoint']['method']) == 'GET' ||
+		   strtoupper($content['endpoint']['method']) == 'POST')
 		{
 			try
 			{   //makes a coonection to the local redis database
@@ -53,14 +69,8 @@ if($content = json_decode(file_get_contents('php://input'), true))
 				//makes separate "postback" objects for each received "data" object
 				foreach($content['data'] as $data)
 				{
-					//redis keeps track of the newest postback object key number in "postback_tail"
-					$id = $redis->incr("postback_tail");
-					//postback keys are in the form "postback:####"
-					$key = 'postback:'.$id;
-					//$redis->hmset($key, 'endpoint', json_encode($content['endpoint']), 'data', json_encode($data));
-					$redis->hset($key, 'endpoint', json_encode($content['endpoint'] + array('data' => $data)));
+					$redis->rpush('request', json_encode($content['endpoint'] + array('data' => $data)));
 					/*
-					echo 'key: '.$key.'<br>';
 					echo '"endpoint": '.json_encode($content['endpoint']).'<br>';
 					echo '"data": '.json_encode($data).'<br><br>';
 					var_dump($content['endpoint']);
@@ -72,6 +82,7 @@ if($content = json_decode(file_get_contents('php://input'), true))
 					echo '<br><br>';
 					*/
 				}
+				echo "Success!<br>";
 			}
 			catch (Exception $e)
 			{
